1d65d36911e3391a377ab5ed90603970b9eb2b68
mrnitj <nithinraj982@gmail.com>
2024-08-05 23:21:55 +0530

    updated

--- src/Pages/Shop/MenShop.tsx
+++ src/Pages/Shop/MenShop.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import MainNavbar from '../../Components/roots/MainNavbar'
-import Banner from '../../Components/Shop/MenShop/Banner'
-import Products from '../../Components/Shop/MenShop/Products'
+import React from "react";
+import MainNavbar from "../../Components/roots/MainNavbar";
+import Banner from "../../Components/Shop/MenShop/Banner";
+import Products from "../../Components/Shop/MenShop/Products";
 
 const MenShop = () => {
-  return (
-    <div>
-        <MainNavbar/>
-        <Banner/>
-        <Products/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <MainNavbar />
+            <Banner />
+            <Products />
+        </div>
+    );
+};
 
-export default MenShop
+export default MenShop;
