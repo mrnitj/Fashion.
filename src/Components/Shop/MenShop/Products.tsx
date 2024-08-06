5bececbdabf8b82497cd85635c585cb36d4f161d
mrnitj <nithinraj982@gmail.com>
2024-08-06 16:03:24 +0530

    feat:: filterBox

--- src/Components/Shop/MenShop/Products.tsx
+++ src/Components/Shop/MenShop/Products.tsx
@@ -1,42 +1,24 @@
 import React from "react";
 import { Box, Breadcrumbs, Container, styled, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import FilterBox from "./FilterBox";
 
 const MainContainer = styled(Box)(({ theme }) => ({
     height: "200vh",
     width: "100%",
 }));
 
-const FilterContainer = styled(Container)(({ theme }) => ({
-    height:'3.6rem',
-    background:'#e7e4e4',
-    display:'flex',
-    alignItems:'center',
-    position:'sticky',
-    top:'4rem',
-    zIndex:1
-}));
-const NewBox = styled(Box)(({ theme }) => ({}));
-const Links = styled(Typography)(({theme})=>({
-    textDecoration:'none',
-    color:'black'
-
-}));
-
-const FiltersBox = styled(Box)(({ theme }) => ({
+const FilterContainer = styled(Box)(({ theme }) => ({
+    position: "sticky",
+    top: "4rem",
+    zIndex: 1,
 }));
 
 const Products = () => {
     return (
         <MainContainer>
-            <FilterContainer maxWidth='xl'>
-                <NewBox>
-                    <Breadcrumbs>
-                        <Links component={Link}>Men</Links>
-                        <Links component={Link}>New</Links>
-                    </Breadcrumbs>
-                </NewBox>
-                <FiltersBox />
+            <FilterContainer>
+                <FilterBox />
             </FilterContainer>
         </MainContainer>
     );
